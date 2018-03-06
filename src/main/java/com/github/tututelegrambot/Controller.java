27b77ca98f05008ad7ad0fcb5df6b6352d5f8e1f
MaximTar <max_131092@mail.ru>
2018-03-06 18:55:42 +0300

    first commit

---
+++ src/main/java/com/github/tututelegrambot/Controller.java
@@ -0,0 +1,4 @@
+package com.github.tututelegrambot;
+
+public class Controller {
+}
---
+++ src/main/java/com/github/tututelegrambot/Main.java
@@ -0,0 +1,20 @@
+package com.github.tututelegrambot;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApiContextInitializer.init();
+
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        try {
+            botsApi.registerBot(new MyAmazingBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
